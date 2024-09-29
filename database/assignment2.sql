e8994f6382cddb56830b3d77ac87b902bf0492f7
Roberto Uriona <roberto.uriona@jalasoft.com>
2024-09-28 23:09:59 -0400

    Completed Assignment

---
+++ database/assignment2.sql
@@ -0,0 +1,30 @@
+-- Task One - Write SQL Statements
+-- 1
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- 2
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+-- 3
+DELETE FROM account
+WHERE account_id = 1;
+
+-- 4
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 5
+SELECT inv_make, inv_model, classification_name
+FROM public.inventory as i
+INNER JOIN public.classification as c
+    ON i.classification_id = c.classification_id
+WHERE classification_name = 'Sport';
+
+-- 6
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');
