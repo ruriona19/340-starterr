a349a99fdba6147080f107d6194434e93615500a
Roberto Uriona <roberto.uriona@jalasoft.com>
2024-10-06 14:03:55 -0400

    Adding MVC design pattern

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
+
 
 /* ***********************
  * View Engine and Templates
@@ -23,10 +26,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-// Index rout
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+// Index route
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
